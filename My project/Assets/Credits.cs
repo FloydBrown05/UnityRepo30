1cc728a370766e6e869b82818f4592c1dee7f89f
FloydBrown05 <floydbrownaz16@gmail.com>
2024-04-23 21:55:19 -0700

    yes31

---
+++ My project/Assets/Credits.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+
+
+    public void Quit()
+    {
+
+        Debug.Log("Quit"); 
+        Application.Quit();
+            
+
+        
+    }
+
+
+}
